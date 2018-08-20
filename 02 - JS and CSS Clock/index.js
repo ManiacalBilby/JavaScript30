435414e5e0c1a0525f706129136810bc5e536f69
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-08-20 15:26:15 -0400

    Added jQuery slim minified CDN. Converted Javascript selectors to jQuery

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -1,23 +1,25 @@
+$(document).ready(function(){
 
-    const secondHand = document.querySelector('.second-hand');
-    const minuteHand = document.querySelector('.min-hand');
-    const hourHand = document.querySelector('.hour-hand');
+  const secondHand = $(".second-hand");
+  const minuteHand = $(".min-hand");
+  const hourHand = $(".hour-hand");
 
-    function setDate() {
-      const now = new Date();
-      const seconds = now.getSeconds()
-      const minutes = now.getMinutes()
-      const hours = now.getHours()
+  function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
 
-      const secondsDegrees = ((seconds / 60) * 360) + 90;
-      const minutesDegrees = ((minutes / 60 * 360) + 90)
-      const hoursDegrees = ((hours / 12) * 360) + 90;
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60 * 360) + 90)
+    const hoursDegrees = ((hours / 12) * 360) + 90;
 
-      secondHand.style.transform = `rotate(${secondsDegrees}deg)`
-      minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
-      hourHand.style.transform = `rotate(${hoursDegrees}deg)`
-      
-      console.log(seconds)
-    }
+    secondHand.css('transform', `rotate(${secondsDegrees}deg)`)
+    minuteHand.css('transform', `rotate(${minutesDegrees}deg)`)
+    hourHand.css('transform', `rotate(${hoursDegrees}deg)`)
+  }
 
-    setInterval(setDate, 1000)
+  setInterval(setDate, 1000)
+
+});
+
